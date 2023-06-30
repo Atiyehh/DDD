04e044ee34e819b678ca4f756acbe0af68bd1be9
atiab <atiab@AtiR>
2023-06-29 21:35:38 -0700

    created

---
+++ oneoscillatorODE.m
@@ -0,0 +1,22 @@
+% Given parameters and data
+tic;
+V = linspace(-0.48, 0.9, 298);
+I = msh_array(1:298, 2) * 1e-3;
+L = 1e-3;
+C =1e-9; 
+tmax=0.01;
+
+ind_0 = find(V >= 0, 1);
+dV=mean(diff(V));
+
+tspan = [0, tmax];
+V_0=0;
+y0 = [V_0; 0];
+dydt = @(t, y) odefunc(t, y, V, I, L, C);
+[t, y] = ode45(dydt, tspan, y0);
+VL = y(:, 1);
+iL = y(:, 2);
+plot(t, VL, 'linewidth', 2);
+xlabel('Time');
+ylabel('VL');
+toc;
